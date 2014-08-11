8115056472de6f97ad24e859b4eb364e73c9b8a3
maximebourdel <bourdel.maxime.pro@gmail.com>
2014-08-11 21:56:16 +0200

    Installation du premier listener pour affichage baniere bêta

--- src/Sdz/BlogBundle/Antispam/SdzAntispam.php
+++ src/Sdz/BlogBundle/Antispam/SdzAntispam.php
@@ -5,6 +5,23 @@ namespace Sdz\BlogBundle\Antispam;
 
 class SdzAntispam extends \Twig_Extension
 {
+    protected $mailer;
+    protected $locale;
+    protected $nbForSpam;
+    
+    // Dans le constructeur, on retire $locale des arguments
+    public function __construct(\Swift_Mailer $mailer, $nbForSpam)
+    {
+        $this->mailer    = $mailer;
+        $this->nbForSpam = (int) $nbForSpam;
+    }
+    
+    // Et on ajoute un setter
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+    
     /*
      * Twig va exécuter cette méthode pour savoir quelle(s) fonction(s) ajoute
      * notre service
